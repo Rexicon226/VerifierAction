0788bf5a070a8946f3492842e31a37a5bb5a0ea8
David <87927264+Rexicon226@users.noreply.github.com>
2023-05-24 20:01:12 -0700

    Update verify_check.py

--- scripts/verify_check.py
+++ scripts/verify_check.py
@@ -49,7 +49,7 @@ if swinfo_count == 1:
         if response.status_code != 200:
             raise ReturnCodeError("The 'version_check' URL in 'swinfo.json' is incorrect or invalid. Please make sure there are no typos and it is a valid link to a swinfo.json or .csproj.")
             
-        print(response)
+        print(response.json())
             
         print("Verify Check was successful as far as we could tell. If any other issues arise, please contact the KSP2 Modding Society Discord.")
 else:

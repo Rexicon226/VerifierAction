4bdaecf0f25c8d5d94556fcda00016741aecb1ba
David <87927264+Rexicon226@users.noreply.github.com>
2023-05-24 19:29:22 -0700

    Update verify_check.py

--- scripts/verify_check.py
+++ scripts/verify_check.py
@@ -6,7 +6,6 @@ import os
 PATH = '/home/runner/work/'
 SWINFO_FILENAME = "swinfo.json"
 CSPROJ_EXTENSION = ".csproj"
-DIRECTORY_BUILD_TARGETS_FILENAME = "Directory.Build.targets"
 VERSION_CHECK_TAG = "<VersionCheckUrl>(.*?)</VersionCheckUrl>"
 
 # Custom Error Classes
@@ -25,14 +24,10 @@ for root, _, files in os.walk(PATH):
     if SWINFO_FILENAME in files:
         swinfo_paths.append(os.path.join(root, SWINFO_FILENAME))
     for file_name in files:
-        if file_name.endswith(CSPROJ_EXTENSION):
-            csproj_paths.append(os.path.join(root, file_name))
-    if DIRECTORY_BUILD_TARGETS_FILENAME in files:
-        build_targets_paths.append(os.path.join(root, DIRECTORY_BUILD_TARGETS_FILENAME))
+        print(file_name)
         
 swinfo_count = len(swinfo_paths)
 csproj_count = len(csproj_paths)
-build_targets_count = len(build_targets_paths)
 
 if swinfo_count == 0 and csproj_count == 0:
     raise ResolutionError("No 'swinfo.json' or '.csproj' files were detected in your project directory. Please make sure there is at least one of them.")
@@ -55,23 +50,7 @@ if swinfo_count == 1:
             
         print("Verify Check was successful as far as we could tell. If any other issues arise, please contact the KSP2 Modding Society Discord.")
 else:
-     for csproj_path, build_targets_path in zip(csproj_paths, build_targets_paths):
+     for csproj_path in zip(csproj_paths):
         with open(csproj_path) as csproj_file:
             csproj_contents = csproj_file.read()
-            if VERSION_CHECK_TAG not in csproj_contents:
-                continue  # Move to the next pair of files
-            
-            with open(build_targets_path) as build_targets_file:
-                build_targets_contents = build_targets_file.read()
-                match = re.search(VERSION_CHECK_TAG, build_targets_contents)
-                if not match:
-                    raise ResolutionError(f"No '{VERSION_CHECK_TAG}' found in '{build_targets_path}'.")
-                
-                check_url = match.group(1).strip()
-                
-                if not check_url:
-                    raise ResolutionError(f"No URL found for '{VERSION_CHECK_TAG}' in '{build_targets_path}'.")
-                
-                response = requests.get(check_url)
-                if response.status_code != 200:
-                    raise ReturnCodeError(f"The URL '{check_url}' from '{build_targets_path}' is incorrect or invalid. Please make sure there are no typos and it is a valid")
+            print("got to the end of the csproj")

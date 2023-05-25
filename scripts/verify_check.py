82c9d65b29998b6fca78ec843079aa29ed02f71d
David <87927264+Rexicon226@users.noreply.github.com>
2023-05-24 19:09:15 -0700

    Update verify_check.py

--- scripts/verify_check.py
+++ scripts/verify_check.py
@@ -1,61 +1,41 @@
-"""
-==========================================
-Author: Rexicon226 <87927264+Rexicon226@users.noreply.github.com>
-Co-authored-by: Mineinjava <65673396+Mineinjava@users.noreply.github.com>
-Date:   20 May 2023
-==========================================
-"""
-
 import json
 import requests
 import os
 
-# Declare Variables
-
+# Declare Constants
 PATH = '/home/runner/work/'
-
-swinfo_amount = 0
-swinfo_path = ""
+SWINFO_FILENAME = "swinfo.json"
 
 # Custom Error Classes
-
 class ResolutionError(Exception):
     pass
 
 class ReturnCodeError(Exception):
     pass
 
-# Recursive search through the root, find swinfo.json
-
-for root, subFolder, files in os.walk(PATH):
-    for item in files:
-        if item == "swinfo.json" :
-            swinfo_amount += 1
-            if swinfo_amount > 1:
-                raise ResolutionError("You have more than one 'swinfo.json' file in your project, please resolve this.")
-            swinfo_path = os.path.join(root, item)
-            
-if swinfo_amount == 0:
+# Find swinfo.json recursively
+swinfo_paths = []
+for root, _, files in os.walk(PATH):
+    if SWINFO_FILENAME in files:
+        swinfo_paths.append(os.path.join(root, SWINFO_FILENAME))
+        
+swinfo_count = len(swinfo_paths)
+if swinfo_count == 0:
     raise ResolutionError("No 'swinfo.json' was detected in your project directory. Please make sure there is one.")
+elif swinfo_count > 1:
+    raise ResolutionError("You have more than one 'swinfo.json' file in your project, please resolve this.")
             
-            
-# Read contents
-
-swinfo_file = open(swinfo_path)   
-
-contents = json.load(swinfo_file)
-
-swinfo_file.close()
-
-# Check validity of URL
-
-check_url = contents.get("version_check")
-
-return_code = requests.get(check_url).status_code
-
-if (return_code != 200):
-    raise ReturnCodeError("The 'version_check' you have put into your 'swinfo.json' is incorrect / invalid. Please make sure there are no typos and it is a valid link to a swinfo.json or .csproj")
+# Read contents and check validity of URL
+swinfo_path = swinfo_paths[0]
+with open(swinfo_path) as swinfo_file:
+    contents = json.load(swinfo_file)
+    check_url = contents.get("version_check")
+    
+    if not check_url:
+        raise ResolutionError("No 'version_check' URL found in 'swinfo.json'.")
+    
+    response = requests.get(check_url)
+    if response.status_code != 200:
+        raise ReturnCodeError("The 'version_check' URL in 'swinfo.json' is incorrect or invalid. Please make sure there are no typos and it is a valid link to a swinfo.json or .csproj.")
       
-# TODO: check if version_check exists and is valid in the endpoint
-
-print("Verify Check was successful as far as we could tell. If any other issues arise, please contact the KSP2 Modding Society Discord.")
+print("Verification was successful. If any other issues arise, please contact the KSP2 Modding Society Discord.")
